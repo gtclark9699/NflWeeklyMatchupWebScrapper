aadf2c905e20977269f75714b2c24cf83121fc21
gtclark9699 <gtclark2021@gmail.com>
2023-09-07 20:27:38 -0700

    Initial code push, basic ui and back end is set up. No connection between the two exists

---
+++ web-scrapper-front-end/src/components/App.js
@@ -0,0 +1,9 @@
+import Dashboard from "./Dashboard/Dashboard";
+
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
---
+++ web-scrapper-front-end/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import LoginPage from "./LoginPage";
+
+
+function Dashboard() {
+
+    return (
+        <LoginPage />
+    )
+}
+
+export default Dashboard;
